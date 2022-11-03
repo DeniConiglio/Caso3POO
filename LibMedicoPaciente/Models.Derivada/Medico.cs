50efe8d3af44f97c91bc84dea8f5681ad80f0b22
DeniConiglio <deniseconiglio2017@outlook.com>
2022-11-03 00:10:32 -0300

    Agregar archivos de proyecto.

---
+++ LibMedicoPaciente/Models.Derivada/Medico.cs
@@ -0,0 +1,23 @@
+﻿using LibMedicoPaciente.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMedicoPaciente.Models.Derivada
+{
+    public class Medico:Persona
+    {
+        public string Especialidad { get; set; }
+        public Medico(string nombre, string especialidad):base(nombre)
+        {
+            Especialidad = especialidad;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + Especialidad;
+        }
+    }
+}
---
+++ LibMedicoPaciente/Models.Derivada/Paciente.cs
@@ -0,0 +1,19 @@
+﻿using LibMedicoPaciente.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibMedicoPaciente;
+
+namespace LibMedicoPaciente.Models.Derivada
+{
+    public class Paciente:Persona
+    {
+            public override string ToString()
+            {
+                return Nombre;
+            }
+        
+    }
+}
